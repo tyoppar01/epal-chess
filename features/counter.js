021371f2af8c713d5c52559f0ccb4be3f0f4204b
jk <jayaw5508@gmail.com>
2025-11-18 20:16:52 +0800

    Rename file only

--- features/counter.js
+++
@@ -1,9 +0,0 @@
-/**
- * Tile Counter (Applicable for both player)
- * @param {*} board 
- * @param {*} playerA 
- */
-export const tileCount = (board, playerA = 1) => {
-    board.flatten().filter(tile => tile === playerA).length;
-} 
-
